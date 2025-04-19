50c253ee7d3cd3fe5fcef1c5074481877f7f2821
Vasco Macedo <saruga19@sapo.pt>
2025-04-19 16:09:17 +0100

    Finished Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let reversedWord =  word.toUpperCase().split('').reverse().join('');
+  return reversedWord === word;
 };
 
 // Do not edit below this line
